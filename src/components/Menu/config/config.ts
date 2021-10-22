5696ee8548e8652afbace55a55e2b1cfea7d294b
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2021-10-22 22:46:05 +0700

    feat(dfh): Xoá những phần thừa ở trên UI: header, footer

--- src/components/Menu/config/config.ts
+++ src/components/Menu/config/config.ts
@@ -1,114 +1,36 @@
-import { MenuItemsType, DropdownMenuItemType } from '@dfh-finance/uikit'
+import { MenuItemsType } from '@dfh-finance/uikit'
 import { ContextApi } from 'contexts/Localization/types'
-import { nftsBaseUrl } from 'views/Nft/market/constants'
 
 export type ConfigMenuItemsType = MenuItemsType & { hideSubNav?: boolean }
 
 const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
   {
-    label: t('Trade'),
+    label: t('Swap'),
     icon: 'Swap',
     href: '/swap',
     showItemsOnMobile: false,
-    items: [
-      {
-        label: t('Exchange'),
-        href: '/swap',
-      },
-      {
-        label: t('Liquidity'),
-        href: '/liquidity',
-      },
-    ],
+    items: [],
   },
   {
-    label: t('Earn'),
+    label: t('Liquidity'),
+    icon: 'Liquidity',
+    href: '/liquidity',
+    showItemsOnMobile: false,
+    items: [],
+  },
+  {
+    label: t('Farms'),
+    icon: 'Farms',
     href: '/farms',
-    icon: 'Earn',
-    items: [
-      {
-        label: t('Farms'),
-        href: '/farms',
-      },
-      {
-        label: t('Pools'),
-        href: '/pools',
-      },
-    ],
+    showItemsOnMobile: false,
+    items: [],
   },
   {
-    label: t('Win'),
-    href: '/prediction',
-    icon: 'Trophy',
-    items: [
-      {
-        label: t('Prediction (BETA)'),
-        href: '/prediction',
-      },
-      {
-        label: t('Lottery'),
-        href: '/lottery',
-      },
-    ],
-  },
-  {
-    label: t('NFT'),
-    href: `${nftsBaseUrl}`,
-    icon: 'Nft',
-    items: [
-      {
-        label: t('Overview'),
-        href: `${nftsBaseUrl}`,
-        status: {
-          text: t('Live'),
-          color: 'failure',
-        },
-      },
-      {
-        label: t('Collections'),
-        href: `${nftsBaseUrl}/collections`,
-      },
-    ],
-  },
-  {
-    label: '',
-    href: '/info',
-    icon: 'More',
-    hideSubNav: true,
-    items: [
-      {
-        label: t('Info'),
-        href: '/info',
-      },
-      {
-        label: t('IFO'),
-        href: '/ifo',
-      },
-      {
-        label: t('Voting'),
-        href: '/voting',
-      },
-      {
-        type: DropdownMenuItemType.DIVIDER,
-      },
-      {
-        label: t('Leaderboard'),
-        href: '/teams',
-      },
-      {
-        type: DropdownMenuItemType.DIVIDER,
-      },
-      {
-        label: t('Blog'),
-        href: 'https://medium.com/pancakeswap',
-        type: DropdownMenuItemType.EXTERNAL_LINK,
-      },
-      {
-        label: t('Docs'),
-        href: 'https://docs.pancakeswap.finance',
-        type: DropdownMenuItemType.EXTERNAL_LINK,
-      },
-    ],
+    label: t('Pools'),
+    icon: 'Pools',
+    href: '/pools',
+    showItemsOnMobile: false,
+    items: [],
   },
 ]
 

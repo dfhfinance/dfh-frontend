a31c73351e1e9a55512f9b3bb64fc59f2fc16d56
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2021-10-31 12:43:40 +0700

    feat(dfh): Modal's thing

--- src/components/Menu/GlobalSettings/SettingsModal.tsx
+++ src/components/Menu/GlobalSettings/SettingsModal.tsx
@@ -1,15 +1,9 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { Text, PancakeToggle, Toggle, Flex, Modal, InjectedModalProps, ThemeSwitcher } from '@dfh-finance/uikit'
-import {
-  useAudioModeManager,
-  useExpertModeManager,
-  useUserExpertModeAcknowledgementShow,
-  useUserSingleHopOnly,
-} from 'state/user/hooks'
+import { Flex, InjectedModalProps, Modal, Text, Toggle } from '@dfh-finance/uikit'
+import { useExpertModeManager, useUserExpertModeAcknowledgementShow, useUserSingleHopOnly } from 'state/user/hooks'
 import { useTranslation } from 'contexts/Localization'
 import { useSwapActionHandlers } from 'state/swap/hooks'
-import useTheme from 'hooks/useTheme'
 import QuestionHelper from '../../QuestionHelper'
 import TransactionSettings from './TransactionSettings'
 import ExpertModal from './ExpertModal'
@@ -28,11 +22,8 @@ const SettingsModal: React.FC<InjectedModalProps> = ({ onDismiss }) => {
   const [showExpertModeAcknowledgement, setShowExpertModeAcknowledgement] = useUserExpertModeAcknowledgementShow()
   const [expertMode, toggleExpertMode] = useExpertModeManager()
   const [singleHopOnly, setSingleHopOnly] = useUserSingleHopOnly()
-  const [audioPlay, toggleSetAudioMode] = useAudioModeManager()
   const { onChangeRecipient } = useSwapActionHandlers()
-
   const { t } = useTranslation()
-  const { theme, isDark, toggleTheme } = useTheme()
 
   if (showConfirmExpertModal) {
     return (
@@ -57,27 +48,12 @@ const SettingsModal: React.FC<InjectedModalProps> = ({ onDismiss }) => {
   }
 
   return (
-    <Modal
-      title={t('Settings')}
-      headerBackground="gradients.cardHeader"
-      onDismiss={onDismiss}
-      style={{ maxWidth: '420px' }}
-    >
+    <Modal title={t('Settings')} onDismiss={onDismiss} style={{ maxWidth: '420px' }}>
       <ScrollableContainer>
-        <Flex pb="24px" flexDirection="column">
-          <Text bold textTransform="uppercase" fontSize="12px" color="secondary" mb="24px">
-            {t('Global')}
-          </Text>
-          <Flex justifyContent="space-between">
-            <Text mb="24px">{t('Dark mode')}</Text>
-            <ThemeSwitcher isDark={isDark} toggleTheme={toggleTheme} />
+        <Flex flexDirection="column">
+          <Flex mb="24px">
+            <GasSettings />
           </Flex>
-          <GasSettings />
-        </Flex>
-        <Flex pt="24px" flexDirection="column" borderTop={`1px ${theme.colors.cardBorder} solid`}>
-          <Text bold textTransform="uppercase" fontSize="12px" color="secondary" mb="24px">
-            {t('Swaps & Liquidity')}
-          </Text>
           <TransactionSettings />
         </Flex>
         <Flex justifyContent="space-between" alignItems="center" mb="24px">
@@ -91,7 +67,7 @@ const SettingsModal: React.FC<InjectedModalProps> = ({ onDismiss }) => {
           </Flex>
           <Toggle id="toggle-expert-mode-button" scale="md" checked={expertMode} onChange={handleExpertModeToggle} />
         </Flex>
-        <Flex justifyContent="space-between" alignItems="center" mb="24px">
+        <Flex justifyContent="space-between" alignItems="center">
           <Flex alignItems="center">
             <Text>{t('Disable Multihops')}</Text>
             <QuestionHelper text={t('Restricts swaps to direct pairs only.')} placement="top-start" ml="4px" />
@@ -104,17 +80,6 @@ const SettingsModal: React.FC<InjectedModalProps> = ({ onDismiss }) => {
               setSingleHopOnly(!singleHopOnly)
             }}
           />
-        </Flex>
-        <Flex justifyContent="space-between" alignItems="center">
-          <Flex alignItems="center">
-            <Text>{t('Flippy sounds')}</Text>
-            <QuestionHelper
-              text={t('Fun sounds to make a truly immersive pancake-flipping trading experience')}
-              placement="top-start"
-              ml="4px"
-            />
-          </Flex>
-          <PancakeToggle checked={audioPlay} onChange={toggleSetAudioMode} scale="md" />
         </Flex>
       </ScrollableContainer>
     </Modal>
